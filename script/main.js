bbd5196060d810bd1abf379ba0cd5250149d6fb1
Franci Melink <francimelink@gmail.com>
2023-08-04 10:43:27 +0200

    Move to Vue

--- script/main.js
+++ script/main.js
@@ -1,53 +1,63 @@
-const lightButton = document.getElementById('themeLight')
-const darkButton = document.getElementById('themeDark')
-const searchForm = document.querySelector("form")
-const user = document.getElementById('user')
+
+const app = Vue.createApp({
+    data() {
+        return {
+            userAvatarImg: '',
+            userName: '',
+            userAt: '',
+            userJoined: '',
+            userDesc: '',
+            userPubRepo: 0,
+            userFollowers: 0,
+            userFollowing: 0,
+            userLocation: '',
+            userWebsite: '',
+            userTwitter: '',
+            userCompany: ''
+        }
+    },
+    methods: {
+        updateUser: function (userData) {
+            const user = document.getElementById('user')
+            this.userAvatarImg = userData.avatar_url
+            this.userName = userData.name;
+            this.userAt = `@${userData.login}`;
+            this.userJoined = `Joined ${userData.created_at}`
 
 
-lightButton.addEventListener('click', switchTheme)
-darkButton.addEventListener('click', switchTheme)
+            console.log(this.userAvatarImg);
 
+            //user.classList.add('visible')
+        },
+        switchTheme: function () {
+            const lightButton = document.getElementById('themeLight')
+            const darkButton = document.getElementById('themeDark')
 
-function switchTheme() {
-    lightButton.classList.toggle('hide')
-    darkButton.classList.toggle('hide')
-    document.body.classList.toggle('dark')
-    document.body.classList.toggle('light')
-}
+            lightButton.classList.toggle('hide')
+            darkButton.classList.toggle('hide')
+            document.body.classList.toggle('dark')
+            document.body.classList.toggle('light')
+        },
 
-searchForm.addEventListener('submit', function (event) {
-    event.preventDefault()
-    let gitUsername = document.getElementById('search')
-    gitUsername = gitUsername.value
+        searchUser: function () {
+            let gitUsername = document.getElementById('search')
+            gitUsername = gitUsername.value
 
-    let url = `https://api.github.com/users/${gitUsername}`
+            let url = `https://api.github.com/users/${gitUsername}`
 
-    fetch(url)
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-            updateUser(data)
-        })
-        .catch(error => {
-            console.error(error)
-        })
+            fetch(url)
+                .then(response => {
+                    return response.json()
+                })
+                .then(data => {
+                    this.updateUser(data)
+                })
+                .catch(error => {
+                    console.error(error)
+                })
+        }
+
+    }
 })
 
-function updateUser(userData) {
-    console.log(userData);
-    let userAvatarImg = document.querySelector('.user__avatar-img')
-    let userName = document.querySelector('.user__data-name')
-    let userAt = document.querySelector('.user__data-at')
-    let userJoined = document.querySelector('.user__data-joined')
-
-    userAvatarImg.setAttribute('src', userData.avatar_url)
-    userName.textContent = userData.name;
-    userAt.textContent = `@${userData.login}`;
-    userJoined.textContent = `Joined ${userData.created_at}`
-
-    user.classList.add('visible')
-
-}
-
-
+app.mount('#app')
